2d2ed514f0dcff940d0271f5a2c3d0daa7c69285
spininador <up201405079@fe.up.pt>
2017-03-31 12:12:40 +0100

    D

--- Trench/src2/Board.cpp
+++ Trench/src2/Board.cpp
@@ -5,7 +5,7 @@ Board::Board(){
 		{EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        ,BLACK|SRG    ,BLACK|CPT    ,BLACK|LIT    ,BLACK|GEN    },
 		{EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        ,BLACK|SLD    ,BLACK|SRG    ,BLACK|CPT    ,BLACK|LIT    },
 		{EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        ,BLACK|SLD    ,BLACK|SLD    ,BLACK|SRG    ,BLACK|CPT    },
-		{EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        ,BLACK|SLD    ,BLACK|SLD    ,BLACK|SLD    ,BLAC|SRG     },
+		{EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        ,BLACK|SLD    ,BLACK|SLD    ,BLACK|SLD    ,BLACK|SRG    },
 		{WHITE|SRG    ,WHITE|SLD    ,WHITE|SLD    ,WHITE|SLD    ,EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        },
 		{WHITE|CPT    ,WHITE|SRG    ,WHITE|SLD    ,WHITE|SLD    ,EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        },
 		{WHITE|LIT    ,WHITE|CPT    ,WHITE|SRG    ,WHITE|SLD    ,EMPTY        ,EMPTY        ,EMPTY        ,EMPTY        },
