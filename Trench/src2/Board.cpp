ebe5526ac9c47ad2baba988e43dfeb6ce88008ca
nuno <nunojorge90@hotmail.com>
2017-05-11 00:37:28 +0100

    done?

--- Trench/src2/Board.cpp
+++ Trench/src2/Board.cpp
@@ -57,46 +57,67 @@ std::vector<Move> Board::getPieceMoves(MCoord x, MCoord y) {
 	std::vector<Move> ret;
 	//Get piece at coords
 	Piece p = board[y][x];
+	//Checks if piece is at the trench
 	bool atTrench = x == y;
+	//Checks if piece is in it's territory(not used if in trench)
 	bool atFriendlyTerritory = (p & BLACK && x > y) || (p & WHITE && x < y);
+	//Length the piece can move
 	MLength maxLength = Move::getMaxLength(p);
 
 	
 	for (MAngle angles = 0; angles < 8; angles++) {
 		for (MLength length = 0; length < maxLength; length++) {
 			if (Move::legalAngle(angles, p)) {
+				//Get destiny coords
 				MCoord ydest = y + Move::getAngleY(angles);
 				MCoord xdest = x + Move::getAngleX(angles);
+				//Check if outside boundaries
 				if (ydest < 0 || xdest < 0 || ydest > 7 || xdest > 7) {
 					continue;
 				}
+				//Get destiny
 				Piece p2 = board[ydest][xdest];
-				//If it's a soldier
-				if (p & SLD) {
-
-				}
-				else
-				//If it's a seargent
-				if (p & SRG) {
-
-				}
-				else
-				//If it's a captain
-				if (p & CPT) {
-						
-				}
-				else
-				//If it's a lieutenant
-				if (p & LIT) {
-
-				}
-				else
-				//If it's a general
-				if (p & GEN)
+				//If destiny is empty
+				if (p2 & EMPTY) {
+					Move move = Move(x, y, xdest, ydest);
+					ret.push_back(move);
+					continue;
+				}else
+				//If destiny is friend
+				if (p & BLACK && p2 & BLACK || p & WHITE && p2 & WHITE) {
+					break;
+				}else
+				//If destiny is enemy
 				{
-
+					//If both target and origin are trench
+					if (atTrench && xdest == ydest) {
+						break;
+					}
+					//If target is in enemy field and current in trench
+					else if (atTrench && (p & BLACK && xdest < ydest) || (p & WHITE && xdest > ydest)) {
+						Move move = Move(x, y, xdest, ydest);
+						ret.push_back(move);
+						//Continue pois pode comer mais peças na mesma linha caso esteja na trincheira
+						continue;
+					}
+					//If target is in ally field and current in trench
+					else if (atTrench && (p & BLACK && xdest > ydest) || (p & WHITE && xdest < ydest)) {
+						break;
+					}
+					else if (!atTrench && xdest == ydest && atFriendlyTerritory) {
+						break;
+					}
+					else if (!atTrench && xdest == ydest && !atFriendlyTerritory) {
+						Move move = Move(x, y, xdest, ydest);
+						ret.push_back(move);
+						break;
+					}
+					else if (!atTrench && xdest != ydest) {
+						Move move = Move(x, y, xdest, ydest);
+						ret.push_back(move);
+						break;
+					}
 				}
-
 			}
 		}
 	}
@@ -131,8 +152,6 @@ MCoord Move::getDY() {
 Piece Board::getPiece(MCoord y, MCoord x) {
 	return this->board[y][x];
 }
-
-/////////////////////////////// ANGLE ////////////////////////////////
 
 MCoord Move::getAngleX(MAngle angle) {
 	if (angle == 0 || angle == 1 || angle == 7) {
@@ -197,7 +216,7 @@ bool Move::legalAngle(MAngle angle, Piece p) {
 }
 
 MLength Move::getMaxLength(Piece p) {
-	//If it's a soldie
+	//If it's a soldier
 	if (p & SLD) {
 		return 1;
 	}
