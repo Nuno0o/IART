39028c97cd06a953d22b54500598bfcfa04e7005
nuno <nunojorge90@hotmail.com>
2017-05-17 19:27:45 +0100

    d

--- Trench/src2/Source.cpp
+++ Trench/src2/Source.cpp
@@ -3,22 +3,8 @@
 #include "Interface.h"
 #include <time.h>
 
-
-void listMoves(std::vector<Move> moves) {
-	for (int i = 0; i < moves.size(); i++) {
-		std::cout << "Move:(" << (int)moves[i].getSX() << "," << (int)moves[i].getSY() << ") to (" << (int)moves[i].getDX() << "," << (int)moves[i].getDY() << ")\n";
-	}
-}
 int main() {
 	srand(time(0));
 	Interface();
     return 0;
-	//Board b = Board();
-	//printBoard(b);
-	//listMoves(b.getAllMoves(White));
-	//listMoves(b.getAllMoves(Black));
-	//Board b2 = Algorithms::negamaxWithoutABP(b,3,White);
-	//Board b2 = b.movePiece(b.getAllMoves(Black)[6]);
-	//printBoard(b2);
-
 }
